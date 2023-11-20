8866d287c103ea2458db63d1e259f33a0b103788
janicejay <janicejay5@gmail.com>
2023-11-20 13:43:07 -0800

    done

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    output = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
+            else:
+                output += a ** b / i
+        except:
+            output = b + a
+            break
+    return (output)
