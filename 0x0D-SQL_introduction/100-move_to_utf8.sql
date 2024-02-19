ced8bc3078e126658fda9c604a8ae30055db433f
janicejay <janicejay5@gmail.com>
2024-02-19 06:03:07 -0800

    sql files

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+-- Converts all of the following to UTF8;
+-- Database hbtn_0c_0
+-- Table first_table
+-- Field name in first_table
+
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
